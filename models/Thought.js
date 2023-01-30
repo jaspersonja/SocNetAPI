eb2d691e69c397d9e6eb4f20fdc80999e97c5bf4
unknown <jacob.jasperson@yahoo.com>
2023-01-30 00:54:08 -0700

    added boilerplates and fully built models

---
+++ models/Thought.js
@@ -0,0 +1,71 @@
+const { Schema, model } = require('mongoose');
+const moment = require('moment')
+
+// create Thought schema
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      min_length: 1,
+      max_length: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      get: createdAtDay => moment(createdAtDay).format('MM DD, YYYY')
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+//create Reaction schema
+
+const reactionSchema = new Schema (
+  {
+     reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+     },
+     reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280
+     },
+     username: {
+      type: String,
+      required: true,
+     },
+     createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtDay => moment(createdAtDay).format('MM DD, YYYY'),
+     },
+  },
+  {
+      toJSON: {
+          virtuals: true,
+          getters: true
+      },
+      id: false,
+  }
+)
+
+thoughtSchema.virtual('reactionCount').get( function() {
+  return this.reactions.length;
+});
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create a user model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trimmed: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [
+          /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+          "Please enter a valid email address!",
+        ],
+    },
+    thoughts: [{
+      type: Schema.Types.ObjectId,
+      ref: 'Thought',
+    },],
+    friends: [{
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    },],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
